cd032d55a8575ee63d6f59f002cba7bd7b8268e7
wolanski_mateusz <94111142+wolanskimateusz@users.noreply.github.com>
2024-04-16 15:56:22 +0200

    styles

--- frontend/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
+++ frontend/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -10,7 +10,9 @@ const DeletePortfolio = ({onPortfolioDelete,portfolioValue}: Props) => {
   return <>
   <form onSubmit={onPortfolioDelete}>
   <input readOnly={true} hidden={true} value={portfolioValue}></input>
-    <button type='submit'> X </button>
+  <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
+          X
+        </button>
   </form>
   </>
     
